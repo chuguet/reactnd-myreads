2b5fc60f6fda166fd9e2c45afa086aac34695c32
Carlos Huguet <huguet10@gmail.com>
2017-07-23 17:50:21 +0200

    Refactor in components

---
+++ src/ListBooks.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import BookShelf from './BookShelf';
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf/>
+            <BookShelf/>
+            <BookShelf/>
+          </div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks
